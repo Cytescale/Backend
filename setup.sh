75c2d23352e5fb6145303a3b6efee76e9a604c43
cytescale <nikhil@cytescale.com>
2023-02-25 22:10:48 +0530

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 echo ">>Installing NodeJs"
-sudo apt install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+echo "Checking Node Version"
+node -e "console.log('Running Node.js ' + process.version)"
 echo ">>Stashing Branch Changes"
 git stash
 echo ">>Pulling Latest Branch Changes"
